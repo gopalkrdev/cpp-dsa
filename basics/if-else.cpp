22e0738a64f1f9814ce9180099ebd64312f5480f
Gopal Kumar <gopalkumar977156@gmail.com>
2026-08-09 22:56:19 +0530

    Add if-else example for number classification

---
+++ basics/if-else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int number;
+cout << "Enter a number: ";
+cin >> number
+  if (number>0) {
+cout <<"positive number";
+} else if (number <0) {
+cout << "negative number";
+} else {
+cout << "Zero";
+}
+return 0;
+}
