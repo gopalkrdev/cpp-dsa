dec21c3d5be7d64cfd7033b008c336468582e93a
Gopal Kumar <gopalkumar977156@gmail.com>
2026-08-10 19:33:19 +0530

    Add word counting program in count-words.cpp
    This program counts the number of words in a given sentence input by the user.

---
+++ basics/string/count-words.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+int main(){
+  string str;
+cout<<"Enter a sentence:";
+getline(cin,str);
+stringstream ss(str);
+string word;
+int count=0;
+while(ss>>word){
+count++;
+}
+cout<<"Number of words;"; << cout <<endl;
+return 0;
+}
