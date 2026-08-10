9c5ac146a867c524e3d0e7146005dc439d5e4e13
Gopal Kumar <gopalkumar977156@gmail.com>
2026-08-10 16:37:03 +0530

    Add reverse string program in C++

---
+++ basics/string/reverse-string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <string>
+using namespace std;
+int main(){
+  string str;
+cout<<"Enter a string:";
+getline(cin,str);
+string reversed ="";
+for (int i=str.length()-1;i>=0; i--){
+reversed += str[i];
+}
+cout<<"Reversed string:"<<reversed<<endl;
+return 0;
+}
