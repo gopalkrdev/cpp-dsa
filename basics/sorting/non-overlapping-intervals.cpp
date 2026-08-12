5af8ae4282c076e4019fdb0074dbc5a57c867244
Gopal Kumar <gopalkumar977156@gmail.com>
2026-08-12 12:34:13 +0530

    Add non-overlapping intervals solution in C++
    Implement function to count minimum intervals to remove for non-overlapping intervals.

---
+++ basics/sorting/non-overlapping-intervals.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+    if (intervals.empty())
+        return 0;
+
+    // Sort by ending time
+    sort(intervals.begin(), intervals.end(),
+         [](const vector<int>& a, const vector<int>& b) {
+             return a[1] < b[1];
+         });
+
+    int removed = 0;
+    int lastEnd = intervals[0][1];
+
+    for (int i = 1; i < intervals.size(); i++) {
+        if (intervals[i][0] < lastEnd) {
+            // Overlap found
+            removed++;
+        } else {
+            lastEnd = intervals[i][1];
+        }
+    }
+
+    return removed;
+}
+
+int main() {
+    vector<vector<int>> intervals = {
+        {1, 2},
+        {2, 3},
+        {3, 4},
+        {1, 3}
+    };
+
+    cout << "Minimum intervals to remove: "
+         << eraseOverlapIntervals(intervals)
+         << endl;
+
+    return 0;
+}
