86e58dd4cd226f86a2dff9249f19fc7601331507
Gopal Kumar <gopalkumar977156@gmail.com>
2026-08-13 06:38:28 +0530

    Implement maximum subarray sum algorithm in C++

---
+++ basics/arrays/maximum-subarray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int maxSubarraySum(int arr[], int n) {
+    int currentSum = arr[0];
+    int maxSum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        currentSum = max(arr[i], currentSum + arr[i]);
+
+        maxSum = max(maxSum, currentSum);
+    }
+
+    return maxSum;
+}
+
+int main() {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = 9;
+
+    cout << "Maximum subarray sum: "
+         << maxSubarraySum(arr, n)
+         << endl;
+
+    return 0;
+}
