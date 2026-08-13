a194c5e6fcce920f586f8d2ac971c4605b177bec
Gopal Kumar <gopalkumar977156@gmail.com>
2026-08-13 06:42:36 +0530

    Add solution for Best Time to Buy and Sell Stock II

---
+++ basics/arrays/best-time-buy-sell-stock-ii.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int maxProfit(int prices[], int n) {
+    int profit = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+
+    return profit;
+}
+
+int main() {
+    int prices[] = {7, 1, 5, 3, 6, 4};
+    int n = 6;
+
+    cout << "Maximum profit: "
+         << maxProfit(prices, n)
+         << endl;
+
+    return 0;
+}
