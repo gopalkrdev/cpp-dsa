32ead88ddd1f857c3956763a6325a8749c399c5d
Gopal Kumar <gopalkumar977156@gmail.com>
2026-08-24 14:34:09 +0530

    Add Floyd-Warshall algorithm implementation
    Implement Floyd-Warshall algorithm for all-pairs shortest paths.

---
+++ basics/graphs/floyd-warshall.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int INF = 1e9;
+
+void floydWarshall(vector<vector<int>>& dist) {
+
+    int n = dist.size();
+
+    for (int k = 0; k < n; k++) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if (dist[i][k] == INF ||
+                    dist[k][j] == INF) {
+                    continue;
+                }
+
+                dist[i][j] = min(
+                    dist[i][j],
+                    dist[i][k] + dist[k][j]
+                );
+            }
+        }
+    }
+}
+
+int main() {
+
+    int n = 4;
+
+    vector<vector<int>> dist = {
+        {0,   5,   INF, 10},
+        {INF, 0,   3,   INF},
+        {INF, INF, 0,   1},
+        {INF, INF, INF, 0}
+    };
+
+    floydWarshall(dist);
+
+    cout << "All-Pairs Shortest Distance:\n";
+
+    for (int i = 0; i < n; i++) {
+
+        for (int j = 0; j < n; j++) {
+
+            if (dist[i][j] == INF) {
+                cout << "INF ";
+            }
+            else {
+                cout << dist[i][j] << " ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
