9e722e5fa5f910983196aaba416f4c9a68fb98e9
Gopal Kumar <gopalkumar977156@gmail.com>
2026-08-24 14:37:55 +0530

    Implement bridge-finding algorithm in C++
    This implementation finds and prints all bridges in a given undirected graph using Depth First Search (DFS). It maintains discovery and low values to identify edges that, if removed, would increase the number of connected components in the graph.

---
+++ basics/graphs/bridges.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int timer = 0;
+
+void dfs(
+    int node,
+    int parent,
+    vector<vector<int>>& graph,
+    vector<bool>& visited,
+    vector<int>& tin,
+    vector<int>& low
+) {
+    visited[node] = true;
+
+    tin[node] = low[node] = timer++;
+
+    for (int neighbour : graph[node]) {
+
+        // Parent edge
+        if (neighbour == parent) {
+            continue;
+        }
+
+        // Back edge
+        if (visited[neighbour]) {
+
+            low[node] =
+                min(low[node], tin[neighbour]);
+        }
+
+        // Unvisited neighbour
+        else {
+
+            dfs(
+                neighbour,
+                node,
+                graph,
+                visited,
+                tin,
+                low
+            );
+
+            low[node] =
+                min(low[node], low[neighbour]);
+
+            // Bridge condition
+            if (low[neighbour] > tin[node]) {
+
+                cout << "Bridge: "
+                     << node << " - "
+                     << neighbour << endl;
+            }
+        }
+    }
+}
+
+void findBridges(
+    vector<vector<int>>& graph
+) {
+    int n = graph.size();
+
+    vector<bool> visited(n, false);
+
+    vector<int> tin(n, -1);
+    vector<int> low(n, -1);
+
+    for (int i = 0; i < n; i++) {
+
+        if (!visited[i]) {
+
+            dfs(
+                i,
+                -1,
+                graph,
+                visited,
+                tin,
+                low
+            );
+        }
+    }
+}
+
+int main() {
+
+    int n = 5;
+
+    vector<vector<int>> graph(n);
+
+    // 0 -- 1
+    //      |
+    //      2
+    //     / \
+    //    3---4
+
+    graph[0].push_back(1);
+    graph[1].push_back(0);
+
+    graph[1].push_back(2);
+    graph[2].push_back(1);
+
+    graph[2].push_back(3);
+    graph[3].push_back(2);
+
+    graph[2].push_back(4);
+    graph[4].push_back(2);
+
+    graph[3].push_back(4);
+    graph[4].push_back(3);
+
+    findBridges(graph);
+
+    return 0;
+}
