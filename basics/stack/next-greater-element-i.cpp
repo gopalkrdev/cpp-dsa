6570ab5c4358cf05d159bd3469e21735864f2072
Gopal Kumar <gopalkumar977156@gmail.com>
2026-08-25 09:59:12 +0530

    Add next greater element function in C++
    Implement next greater element function using stack.

---
+++ basics/stack/next-greater-element-i.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> nextGreaterElement(
+    vector<int>& nums1,
+    vector<int>& nums2
+) {
+    unordered_map<int, int> nextGreater;
+
+    stack<int> st;
+
+    // Find next greater for every element in nums2
+    for (int x : nums2) {
+
+        while (!st.empty() &&
+               x > st.top()) {
+
+            nextGreater[st.top()] = x;
+            st.pop();
+        }
+
+        st.push(x);
+    }
+
+    // Elements remaining in stack
+    // don't have a greater element
+    while (!st.empty()) {
+
+        nextGreater[st.top()] = -1;
+        st.pop();
+    }
+
+    // Build answer for nums1
+    vector<int> result;
+
+    for (int x : nums1) {
+        result.push_back(nextGreater[x]);
+    }
+
+    return result;
+}
+
+int main() {
+
+    vector<int> nums1 = {
+        4, 1, 2
+    };
+
+    vector<int> nums2 = {
+        1, 3, 4, 2
+    };
+
+    vector<int> result =
+        nextGreaterElement(nums1, nums2);
+
+    cout << "Answer: ";
+
+    for (int x : result) {
+        cout << x << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
