b0eb544266b95cd77561eff3587930bafa7f2d3b
Gopal Kumar <gopalkumar977156@gmail.com>
2026-08-25 09:52:32 +0530

    Add majority element algorithm in C++

---
+++ basics/hashing/majority-element.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+
+    unordered_map<int, int> freq;
+
+    int n = nums.size();
+
+    for (int x : nums) {
+
+        freq[x]++;
+
+        if (freq[x] > n / 2) {
+            return x;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+
+    vector<int> nums = {
+        2, 2, 1, 1, 1, 2, 2
+    };
+
+    cout << "Majority Element: "
+         << majorityElement(nums)
+         << endl;
+
+    return 0;
+}
