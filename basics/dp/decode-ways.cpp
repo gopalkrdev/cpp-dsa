06b7dc447c623f73c75b85e725a0dbee7ec5d014
Gopal Kumar <gopalkumar977156@gmail.com>
2026-09-16 22:38:20 +0530

    Improve comments in decode-ways.cpp
    Refactor comments for clarity in decode-ways.cpp

--- basics/dp/decode-ways.cpp
+++ basics/dp/decode-ways.cpp
@@ -13,20 +13,16 @@ int numDecodings(string s) {
 
     vector<int> dp(n + 1, 0);
 
-    // Empty string has one way
     dp[0] = 1;
 
-    // First character
     dp[1] = 1;
 
     for (int i = 2; i <= n; i++) {
 
-        // Take one digit
         if (s[i - 1] != '0') {
             dp[i] += dp[i - 1];
         }
 
-        // Take two digits
         int number =
             (s[i - 2] - '0') * 10 +
             (s[i - 1] - '0');
