b7ad804e56db70b0bafa8c04053b217f23488783
Gopal Kumar <gopalkumar977156@gmail.com>
2026-09-17 22:28:21 +0530

    Clean up comments in matrix chain multiplication
    Removed comments for clarity in the matrix chain multiplication implementation.

--- basics/dp/matrix-chain-multiplication.cpp
+++ basics/dp/matrix-chain-multiplication.cpp
@@ -5,11 +5,9 @@ using namespace std;
 
 int matrixChainMultiplication(int arr[], int n) {
 
-    // dp[i][j] = minimum cost to multiply
-    // matrices from i to j
+ 
     vector<vector<int>> dp(n, vector<int>(n, 0));
 
-    // length = chain length
     for (int length = 2; length < n; length++) {
 
         for (int i = 1; i < n - length + 1; i++) {
@@ -35,11 +33,7 @@ int matrixChainMultiplication(int arr[], int n) {
 
 int main() {
 
-    // Matrices:
-    // A1 = 10 x 20
-    // A2 = 20 x 30
-    // A3 = 30 x 40
-    // A4 = 40 x 30
+   
 
     int arr[] = {10, 20, 30, 40, 30};
 
