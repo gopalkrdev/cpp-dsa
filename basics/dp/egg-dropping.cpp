4996b0a428e14245305e099f41d87ae5e0245da3
Gopal Kumar <gopalkumar977156@gmail.com>
2026-09-19 17:47:22 +0530

    Improve comments in eggDropping function
    Refactor comments in eggDropping function for clarity.

--- basics/dp/egg-dropping.cpp
+++ basics/dp/egg-dropping.cpp
@@ -6,22 +6,19 @@ using namespace std;
 
 int eggDropping(int eggs, int floors) {
 
-    // dp[e][f] = minimum attempts needed
-    // with e eggs and f floors
+   
     vector<vector<int>> dp(
         eggs + 1,
         vector<int>(floors + 1, 0)
     );
 
-    // 0 floors -> 0 attempts
-    // 1 floor  -> 1 attempt
+    
     for (int e = 1; e <= eggs; e++) {
         dp[e][0] = 0;
         dp[e][1] = 1;
     }
 
-    // With only 1 egg, we have to try
-    // every floor from bottom to top
+    
     for (int f = 1; f <= floors; f++) {
         dp[1][f] = f;
     }
@@ -32,15 +29,11 @@ int eggDropping(int eggs, int floors) {
 
             dp[e][f] = INT_MAX;
 
-            // Try dropping from every floor
             for (int x = 1; x <= f; x++) {
 
-                // Egg breaks:
-                // check floors below
                 int eggBreaks = dp[e - 1][x - 1];
 
-                // Egg doesn't break:
-                // check floors above
+               
                 int eggSurvives = dp[e][f - x];
 
                 int attempts =
