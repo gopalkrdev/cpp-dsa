2a15d130cfe597b88d522b02d4018f3e61c14ebb
Gopal Kumar <gopalkumar977156@gmail.com>
2026-09-20 18:13:38 +0530

    Clean up comments in distinct subsequences.cpp
    Removed comments for clarity in the distinct subsequences algorithm.

--- basics/dp/distinct-subsequences.cpp
+++ basics/dp/distinct-subsequences.cpp
@@ -13,8 +13,6 @@ long long numDistinct(string s, string t) {
         vector<long long>(m + 1, 0)
     );
 
-    // Empty target can always be formed
-    // by choosing nothing.
     for (int i = 0; i <= n; i++) {
         dp[i][0] = 1;
     }
@@ -23,14 +21,10 @@ long long numDistinct(string s, string t) {
 
         for (int j = 1; j <= m; j++) {
 
-            // If characters are different,
-            // we cannot use s[i-1].
+            
             dp[i][j] = dp[i - 1][j];
 
-            // If characters are same,
-            // we have two choices:
-            // 1. Use this character
-            // 2. Skip this character
+            
             if (s[i - 1] == t[j - 1]) {
 
                 dp[i][j] += dp[i - 1][j - 1];
